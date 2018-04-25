5ed5302475e7aa1348360039b33cdbacdc5b7841
石尧 <shiyao@xin.com>
2018-04-25 17:21:23 +0800

    Add NetUtils

--- Demo/src/main/java/com/common/shy/TestActivity.kt
+++ Demo/src/main/java/com/common/shy/TestActivity.kt
@@ -2,7 +2,9 @@ package com.common.shy
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import com.common.shy.commonutils.ListUtils
+import com.common.shy.commonutils.NetUtils
 import com.common.shy.commonutils.StringUtils
 
 class TestActivity : AppCompatActivity() {
@@ -12,5 +14,6 @@ class TestActivity : AppCompatActivity() {
         setContentView(R.layout.activity_test)
         StringUtils.isEmpty("aa")
         ListUtils.removeDuplicateWithOrder(null)
+        Log.e("111111111","="+NetUtils.isNetAvailable(this))
     }
 }
