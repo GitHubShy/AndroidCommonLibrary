cb25b0a2921792edbc2b3125159f4b7d07bbdd48
石尧 <shiyao@xin.com>
2018-04-25 15:24:12 +0800

    添加上床jcenter

--- CommonUtils/src/main/java/com/common/shy/commonutils/ListUtils.java
+++ CommonUtils/src/main/java/com/common/shy/commonutils/ListUtils.java
@@ -1,18 +1,37 @@
 package com.common.shy.commonutils;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 
 public class ListUtils {
 
     /**
      * Determine whether the string is empty
-     *
-     * @param list
-     * @return
      */
     public static boolean isEmpty(List list) {
         if (list == null || list.isEmpty()) return true;
         return false;
+    }
+
+    /**
+     * Delete the duplicate element,keep the order
+     */
+    public static void removeDuplicateWithOrder(List list) {
+        if (list == null) {
+            return;
+        }
+        Set set = new HashSet();
+        List newList = new ArrayList();
+        for (Iterator iter = list.iterator(); iter.hasNext(); ) {
+            Object element = iter.next();
+            if (set.add(element))
+                newList.add(element);
+        }
+        list.clear();
+        list.addAll(newList);
     }
 }
--- Demo/src/main/java/com/common/shy/TestActivity.kt
+++ Demo/src/main/java/com/common/shy/TestActivity.kt
@@ -1,7 +1,8 @@
 package com.common.shy
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.common.shy.commonutils.ListUtils
 import com.common.shy.commonutils.StringUtils
 
 class TestActivity : AppCompatActivity() {
@@ -10,5 +11,6 @@ class TestActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
         StringUtils.isEmpty("aa")
+        ListUtils.removeDuplicateWithOrder(null)
     }
 }
